6ce626d8193d578c6e8423546b4ce97a5e5c5719
Sakib <sakib@local>
2026-02-23 13:28:56 -0500

    chore: ignore generated artifacts
    Remove tracked Next.js build output and ignore .next and .env so the repository stays clean and secure for ongoing development.

--- apps/web/.next/static/chunks/pages/_app-fe0e5d5446c52497.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{710:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(6221)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(710),n(1007))),_N_E=_.O()}]);
